9521b603b5b826a95b1643669222d402da0b176c
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-10-21 16:13:06 -0400

    init

---
+++ kernel.cu
@@ -0,0 +1,12 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
+
+//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
